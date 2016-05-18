53fe9929d968a0c251d3679411e6f8d986bc9195
Steve Damon <stephen.m.damon@vanderbilt.edu>
2016-05-18 14:23:01 -0500

    a few updates and bug fixes

--- assessor_create_util.py
+++ assessor_create_util.py
@@ -1,8 +1,10 @@
 __author__ = 'damons'
 import psycopg2
+from psycopg2.extensions import AsIs
 import sys
 import re
 import datetime
+
 
 def get_connection(dbname='xnat', user='xnat', password=''):
     """
@@ -31,6 +33,18 @@ def get_cursor(connection):
 
     return connection.cursor()
 
+def get_user_id_by_name(cursor, name):
+    """
+
+    :param cursor:
+    :param name:
+    :return:
+    """
+
+    cursor.execute("""SELECT xdat_user_id from xdat_user where login=%s""", name)
+    result = cursor.fetchone()
+    return result[0]
+
 def get_xdat_change_info_id(cursor):
     """
 
@@ -51,17 +65,6 @@ def get_experimentdata_meta_data_id(cursor):
     result = cursor.fetchall()
     return result[0]
 
-def ie_xnat_deriveddata(cursor, experiment_id):
-    """
-
-    :param cursor:
-    :param experiment_id:
-    :return:
-    """
-    cmd = """SELECT ie_xnat_derivedData((%s),0,FALSE,TRUE,false)"""
-    cursor.execute(cmd, experiment_id)
-    result = cursor.fetchall()
-    return result
 def get_deriveddata_meta_data_id(cursor):
     """
 
@@ -72,18 +75,6 @@ def get_deriveddata_meta_data_id(cursor):
     result = cursor.fetchall()
     return result[0]
 
-def ie_xnat_imageassessordata(cursor, experiment_id):
-    """
-
-    :param cursor:
-    :param experiment_id:
-    :return:
-    """
-    cmd = """SELECT ie_xnat_imageAssessorData((%s),0,FALSE,TRUE,false)"""
-    cursor.execute(cmd, experiment_id)
-    result = cursor.fetchall()
-    return result
-
 def get_imageassesordata_meta_data_id(cursor):
     """
 
@@ -93,18 +84,6 @@ def get_imageassesordata_meta_data_id(cursor):
     cursor.execute("""SELECT nextval('public.xnat_imageassessordata_meta_data_meta_data_id_seq') AS meta_data_id""")
     result = cursor.fetchall()
     return result[0]
-
-def i_proc_genprocdata(cursor, experiment_id):
-    """
-
-    :param cursor:
-    :param experiment_id:
-    :return:
-    """
-    cmd = """SELECT i_proc_genProcData('DEV_VUIISXNAT_E00003',0,FALSE,TRUE,false)"""
-    cursor.execute(cmd, experiment_id)
-    result = cursor.fetchall()
-    return result
 
 def get_procgenprocdata_meta_data_id(cursor):
     """
@@ -176,7 +155,6 @@ def insert_xnat_deriveddata(conn, cursor, deriveddata_info,experiment_id):
 
 def insert_xnat_imageassessordata_meta_data(conn,cursor, xft_version, meta_data_id):
     """
-
     :param cursor:
     :return:
     """
@@ -197,9 +175,8 @@ def insert_xnat_imageassessordata(conn, cursor, meta_data_id, assessor_id, sessi
     cursor.execute(cmd, (meta_data_id, assessor_id, session_id))
     conn.commit()
 
-def insert_proc_genrocdata_meta_data(conn,cursor, xft_version, meta_data_id):
+def insert_proc_genprocdata_meta_data(conn,cursor, xft_version, meta_data_id):
     """
-
     :param cursor:
     :return:
     """
@@ -210,7 +187,7 @@ def insert_proc_genrocdata_meta_data(conn,cursor, xft_version, meta_data_id):
     cursor.execute(cmd, (xft_version, activation_date, row_last_modified, insert_date, meta_data_id))
     conn.commit()
 
-def insert_proc_genrocdata(conn, cursor, meta_data_id, experiment_id):
+def insert_proc_genprocdata(conn, cursor, meta_data_id, experiment_id):
     """
 
     :param cursor:
@@ -233,37 +210,6 @@ def insert_need_inputs_status(conn,cursor,procstatus,proctype, experiment_id):
     cursor.execute(cmd, (procstatus, proctype, experiment_id))
     conn.commit()
 
-def update_proc_genprocdata_meta_data(cursor):
-    """
-
-    :param cursor:
-    :return:
-    """
-    cmd = """UPDATE proc_genProcData_meta_data SET last_modified='2016-04-12 13:50:06.283', modified=1 WHERE meta_data_id=1 """
-
-def insert_xdat_change_info(cursor):
-    """
-
-    :param cursor:
-    :return:
-    """
-    cmd = """INSERT INTO xdat_change_info (xdat_change_info_id,change_user,change_date,event_id) VALUES (805,1,'2016-04-12 13:50:06.022',101)"""
-
-def update_ls_proc_genprocdata(cursor):
-    """
-
-    :param cursor:
-    :return:
-    """
-    cmd = """SELECT update_ls_proc_genProcData('DEV_VUIISXNAT_E00003',1)"""
-
-def i_wkr_workflowdata(cursor):
-    """
-
-    :param cursor:
-    :return:
-    """
-
 def get_next_experiment_id(cursor):
     """
     These IDs should really be some sort of serial8 or something of the like,
@@ -272,12 +218,15 @@ def get_next_experiment_id(cursor):
     :param subject_ids:
     :return: the new ID (in a tuple)
     """
-    cmd = """SELECT DISTINCT id FROM (SELECT id FROM xnat_experimentData WHERE id LIKE 'DEV_VUIISXNAT_E%' UNION SELECT DISTINCT id FROM xnat_experimentData_history WHERE id LIKE 'DEV_VUIISXNAT_E%') SRCHA"""
+    cmd = """SELECT DISTINCT id FROM (SELECT id FROM xnat_experimentData WHERE id LIKE 'DEV_VUIISXNAT99_E%' UNION SELECT DISTINCT id FROM xnat_experimentData_history WHERE id LIKE 'DEV_VUIISXNAT99_E%') SRCHA"""
     cursor.execute(cmd)
     experiment_ids = cursor.fetchall()
     # Sort here rather than using postgres to mimic XNAT
     experiment_ids.sort()
-    last_id = experiment_ids[len(experiment_ids)-1]
+    try:
+        last_id = experiment_ids[len(experiment_ids)-1]
+    except IndexError:
+        last_id=('DEV_VUIISXNAT99_E00000',)
 
     # I don't know if numbers are OK in the XNAT instance name. Thus, split the
     #  '_' and take the last element
@@ -288,8 +237,8 @@ def get_next_experiment_id(cursor):
     tail_of_id.pop(len(tail_of_id)-1)
 
     # I don't know if the zeropadding is a hard limit. This mimics the current install
-    id = "%s_E%05d" % ('_'.join(tail_of_id), int(last_id_digits[0])+1)
-    return (id,)
+    new_id = "%s_E%05d" % ('_'.join(tail_of_id), int(last_id_digits[0])+1)
+    return (new_id,)
 
 
 def get_timestamp():
@@ -305,3 +254,126 @@ def get_timestamp_from_datetime_object(obj):
     :return:
     """
     return obj.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+def check_if_project_exits(cursor, project_id):
+    """
+
+    :param cursor:
+    :param project_id:
+    :return:
+    """
+    cmd = """SELECT id from xnat_projectdata WHERE id=(%s)"""
+    cursor.execute(cmd, (project_id,))
+    res = cursor.fetchall()
+    if res:
+        return True
+    else:
+        return False
+
+
+def check_if_subject_exists(cursor, project_id, subject_label):
+    """
+
+    :param cursor:
+    :param project_id:
+    :param subject_label:
+    :return:
+    """
+    cmd = """SELECT id from xnat_subjectdata where project=(%s) AND label=(%s)"""
+    cursor.execute(cmd, (project_id, subject_label,))
+    res = cursor.fetchall()
+    if res:
+        return True
+    else:
+        return False
+
+def check_if_experiment_exists(cursor, project_id,experiment_label):
+    """
+
+    :param cursor:
+    :param project_id:
+    :param experiment_label:
+    :return:
+    """
+    cmd = """SELECT id from xnat_experimentdata where project=(%s) AND label=(%s)"""
+    cursor.execute(cmd, (project_id, experiment_label,))
+    res = cursor.fetchall()
+    if res:
+        return True
+    else:
+        return False
+
+def check_if_assessor_exists(cursor, project_id, assessor_label):
+    """
+
+    :param cursor:
+    :param project_id:
+    :param assessor_label:
+    :return:
+    """
+    cmd = """SELECT id from xnat_experimentdata where project=(%s) AND label=(%s)"""
+    cursor.execute(cmd, (project_id, assessor_label,))
+    res = cursor.fetchall()
+    if res:
+        return True
+    else:
+        return False
+
+def check_if_xsitype_exists(cursor, xsitype):
+    """
+
+    :param cursor:
+    :param xsitype:
+    :return:
+    """
+    # bad idea to pass in table names. So we have them hard coded
+    cmd = get_statement_from_xsitype(xsitype)
+    if cmd is None:
+        return False
+    else:
+        try:
+            cursor.execute(cmd)
+            return True
+        except psycopg2.ProgrammingError:
+            return False
+
+def get_experiment_id_from_label(cursor, project_id, experiment_label):
+    """
+
+    :param cursor:
+    :param project_id:
+    :param experiment_label:
+    :return:
+    """
+    cmd = """SELECT id FROM xnat_experimentdata where project=(%s) and label=(%s)"""
+    cursor.execute(cmd, (project_id, experiment_label,))
+    result = cursor.fetchone()
+    return result
+
+def execute_wrapper(cursor, template, values):
+    """
+    Wrap the execution of a statement and catch the base class error.
+
+    :param cursor: psycopg2
+    :param template: string template of a SQL statement
+    :param values: tuple of values to insert into the SQL template
+    :return: None
+    """
+    try:
+        cursor.execute(template,values)
+    except psycopg2.Error as e:
+        sys.stderr.write('[ERROR]: Caught psycopg2 error code %s\n' % e.pgcode)
+        sys.stderr.write('[ERROR]: Message is %s\n' % e.pgerror)
+        # TODO
+        # Should we cache everything that is done so we can "undo" it on exception?
+        sys.exit(1)
+
+
+def get_statement_from_xsitype(xsitype):
+    mp = {'proc:genProcData': """SELECT 1 from proc_genprocdata""",
+          'fs:fsData': """SELECT 1 from fs_fsdata"""}
+    if xsitype not in mp.keys():
+        return None
+    else:
+        return mp[xsitype]
+
